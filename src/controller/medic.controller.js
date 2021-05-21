6c9a7eea045e9cc618ff5af2e9ec848e7ccfe4c7
jpmarchan <jmarchanz@autonoma.edu.pe>
2021-05-20 20:35:45 -0500

    remaster se termino mantenimiento de Medicos

--- src/controller/medic.controller.js
+++ src/controller/medic.controller.js
@@ -15,13 +15,31 @@ const getMedicById = async (req, res) =>{
 
 const getPatientsByIdMedic = async (req, res) =>{
     const id = req.params.id
-    const response = await client.query('SELECT name, lastname, dni, email, sex, age, especialidad, status FROM users WHERE id = $1 AND rol = 2', [id])
+    const response = await client.query('SELECT name, lastname, dni, email, sex, age, especialidad, status  FROM users WHERE fkidmedic = $1 AND rol = 1', [id])
     res.status(200).json(response.rows)
 
 }
 
+const updateUserMedic = async (req, res) =>{
+    const id = req.params.id
+    const {name, lastname, email, password, rol, status, dni, sex, age, especialidad} = req.body
+    await client.query('UPDATE  users SET name = $1, email = $2, lastname = $3, dni = $4, sex = $5, age = $6, especialidad= $7 WHERE id = $8 AND rol = 2', [name,
+        email,
+        lastname,
+        dni,
+        sex,
+        age,
+        especialidad,
+        id])
+    res.json({
+        message:'medico actualizado '
+    })
+}
+
+
 module.exports = {
     getMedics,
     getMedicById,
-    getPatientsByIdMedic
+    getPatientsByIdMedic,
+    updateUserMedic
 }
