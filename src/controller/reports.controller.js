5e5811c39f7ac7e3fdda3998cf75694b9a648a2a
jpmarchan <jmarchanz@autonoma.edu.pe>
2021-06-10 18:25:53 -0500

    refactor response reportbyid

--- src/controller/reports.controller.js
+++ src/controller/reports.controller.js
@@ -32,18 +32,31 @@ const getReportByPatient = async (req, res) =>{
 const getReportById = async (req, res) =>{
 
     const id = req.params.id
-    const response = await client.query('SELECT r.id, r.fecha, r.fkidmedico, r.fkidpaciente, r.detail , r.status, r.detail, u.name AS namedoc, u.lastname AS lastnamedoc, rg.detail AS detailgenerate, rg.id AS idreportgenerate, rg.status AS statusgenerate FROM reports_original AS r INNER JOIN users AS u ON u.id = r.fkidmedico INNER JOIN reports_generate AS rg ON rg.fkidrepororiginal = r.id WHERE r.id = $1 LIMIT 1', [id])
-   
+    const response = await client.query('SELECT r.id, r.fecha, r.fkidmedico, r.fkidpaciente, r.detail , r.status,  u.name AS namedoc, u.lastname AS lastnamedoc, rg.detail AS detailgenerate, rg.id AS idreportgenerate, rg.status AS statusgenerate FROM reports_original AS r INNER JOIN users AS u ON u.id = r.fkidmedico INNER JOIN reports_generate AS rg ON rg.fkidrepororiginal = r.id WHERE r.id = $1 ', [id])
         response.rows.map(function(obj){
         var rObj = {};
         const unixTime = obj.fecha;
         const date = new Date(unixTime*1000);
         obj.fecha = date.toLocaleDateString("es-ES")
-        console.log( obj.fecha);
         return rObj;
      });
+
+    let data =
+     {
+        id: response.rows[0].id,
+        fecha: response.rows[0].fecha,
+        fkidmedico: response.rows[0].fkidmedico,
+        fkidpaciente: response.rows[0].fkidpaciente,
+        detail: response.rows[0].detail,
+        status: response.rows[0].status,
+        namedoc: response.rows[0].namedoc,
+        lastnamedoc: response.rows[0].lastnamedoc,
+        detailgenerate: response.rows[0].detailgenerate,
+        idreportgenerate: response.rows[0].idreportgenerate,
+        statusgenerate: response.rows[0].statusgenerate,
+     }
    
-    res.status(200).json(response.rows)
+    res.status(200).json(data)
 
 }
 const WatchByReport = async (req, res) =>{
