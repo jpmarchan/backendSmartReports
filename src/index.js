04fe3f983802064d362accb5a0fc73a5aea9371a
jpmarchan <jmarchanz@autonoma.edu.pe>
2021-06-10 23:47:43 -0500

    feature: se agrego cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express =  require('express')
 const app = express();
+const cors = require('cors')
+
 
 //middlewares
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+
 //routes
 app.use(require('./routes/index'))
+
 
 
 app.listen(80)
