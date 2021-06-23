24b6c5745f7982290598d1a0b06ed95076ed15ba
jpmarchan <jmarchanz@autonoma.edu.pe>
2021-06-22 23:04:09 -0500

    feature: se mopdifico respuesta de asignar pacientes a medicos

--- src/controller/admin.controller.js
+++ src/controller/admin.controller.js
@@ -5,6 +5,7 @@ const asingPatientMedic = async (req, res) =>{
     const {idpatient,idmedic} = req.body
     await client.query('UPDATE  users SET fkidmedic = $1 WHERE id = $2 AND rol = 1', [idmedic, idpatient])
     res.json({
+        status: true,
         message:'medico asignado'
     })
 }
