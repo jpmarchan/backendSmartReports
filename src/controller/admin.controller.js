062c42acde838e5942a1c2b7329c6d3eef3166dd
jpmarchan <jmarchanz@autonoma.edu.pe>
2021-06-22 23:40:37 -0500

    refactor: refactorizacion

--- src/controller/admin.controller.js
+++ src/controller/admin.controller.js
@@ -3,6 +3,7 @@ const  { client } = require ('../conection');
 
 const asingPatientMedic = async (req, res) =>{
     const {idpatient,idmedic} = req.body
+    console.log(idpatient,idmedic )
     await client.query('UPDATE  users SET fkidmedic = $1 WHERE id = $2 AND rol = 1', [idmedic, idpatient])
     res.json({
         status: true,
