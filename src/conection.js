e341acc66fa43d13d0171ab7ba72b6684180b8df
JpMarchan <yenner.marchan.zamora@gmail.com>
2021-11-03 12:50:51 -0500

    update

--- src/conection.js
+++ src/conection.js
@@ -1,7 +1,7 @@
 const pg = require('pg')
 
 
-const conString = "pg://postgres:123456@localhost:5432/smartreports";
+const conString = "pg://@localhost:5432/smartreports";
 const client = new pg.Client(conString);
 const conectioncheck = client.connect();
 
@@ -9,6 +9,5 @@ module.exports = {
     client,
     conectioncheck
   }
-  
   
   
